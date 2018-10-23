b3f8691f0fa8526dafa89a5f5821e2c4a49069e2
Rob the Fish <rafish90@gmail.com>
2018-10-23 11:00:51 -0600

    Add files via upload

---
+++ AD_chtr_DataCombo.py
@@ -0,0 +1,157 @@
+import pandas as pd
+import pyodbc
+import os
+
+##########################
+## DELETE PREVIOUS FILE ##
+##########################
+
+#Delete the 20 day file
+if os.path.exists('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_20.csv'):
+    os.remove('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_20.csv')
+    print('20 deleted')
+else:
+    print('20 day file does not exist')
+
+#Delete the 40 day file
+if os.path.exists('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_40.csv'):
+    os.remove('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_40.csv')
+    print('40 deleted')
+else:
+    print('40 day file does not exist')
+
+#Delete the 60 day file
+if os.path.exists('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_60.csv'):
+    os.remove('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_60.csv')
+    print('60 deleted')
+else:
+    print('60 day file does not exist')
+
+#Delete the 80 day file
+if os.path.exists('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_80.csv'):
+    os.remove('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_80.csv')
+    print('80 deleted')
+else:
+    print('80 day file does not exist')
+
+#Delete the 90 day file
+if os.path.exists('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_90.csv'):
+    os.remove('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_90.csv')
+    print('90 deleted')
+else:
+    print('90 day file does not exist')
+
+#Delete the SuperSet file
+if os.path.exists('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_super.csv'):
+    os.remove('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_super.csv')
+    print('Super(chtr) deleted')
+else:
+    print('Super(chtr) file does not exist')
+
+###################
+## FIRST 20 DAYS ##
+###################
+
+#PULL FROM DATABASE AND CREATE DATAFRAME
+sql_conn = pyodbc.connect('dsn=ConnectR')
+query = "select masdiv, station, tuning_evnt_start_dt, dow, moy, transactions from (SELECT mas.mas_div_desc AS MASDIV, stn.stn_nm AS STATION, tuning_evnt_start_dt, cal.day_of_week_shrt_nm AS DOW, cal.clndr_mo_nm AS MOY, COUNT(am_tuning_evnt_key) AS TRANSACTIONS FROM prd_am_bi_1.am_program_tuning_event_fact ptef INNER JOIN prd_am_bi_1.am_calendar_dim cal ON ptef.tuning_evnt_start_dt = cal.clndr_dt INNER JOIN prd_am_bi_1.am_station_dim stn ON ptef.stn_key = stn.stn_key INNER JOIN prd_am_bi_1.am_mas_division_dim mas ON (ptef.mas_div_key = mas.mas_div_key and cal.CLNDR_SKEY between mas.EFF_START_DAY_KEY and mas.EFF_END_DAY_KEY) WHERE cal.clndr_dt >= current_date - 20 and mas.mas_div_desc in ('Charter') GROUP BY tuning_evnt_start_dt, DOW, MOY, STATION, MASDIV) x;"
+df = pd.read_sql(query, sql_conn)
+
+#SHOW INFORMATION ON DATAFRAME
+count_rows = df.shape[0]
+print(count_rows)
+print(df.head(10))
+
+#SAVE THE DATA PULL AS A CSV
+df.to_csv('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_20.csv')
+print('20 SUCCESSFULLY WRITTEN TO CSV')
+
+#############
+## 40 DAYS ##
+#############
+
+#PULL FROM DATABASE AND CREATE DATAFRAME
+sql_conn = pyodbc.connect('dsn=ConnectR')
+query = "select masdiv, station, tuning_evnt_start_dt, dow, moy, transactions from (SELECT mas.mas_div_desc AS MASDIV, stn.stn_nm AS STATION, tuning_evnt_start_dt, cal.day_of_week_shrt_nm AS DOW, cal.clndr_mo_nm AS MOY, COUNT(am_tuning_evnt_key) AS TRANSACTIONS FROM prd_am_bi_1.am_program_tuning_event_fact ptef INNER JOIN prd_am_bi_1.am_calendar_dim cal ON ptef.tuning_evnt_start_dt = cal.clndr_dt INNER JOIN prd_am_bi_1.am_station_dim stn ON ptef.stn_key = stn.stn_key INNER JOIN prd_am_bi_1.am_mas_division_dim mas ON (ptef.mas_div_key = mas.mas_div_key and cal.CLNDR_SKEY between mas.EFF_START_DAY_KEY and mas.EFF_END_DAY_KEY) WHERE cal.clndr_dt < current_date - 20 and cal.clndr_dt >= current_date - 40 and mas.mas_div_desc in ('Charter') GROUP BY tuning_evnt_start_dt, DOW, MOY, STATION, MASDIV) x;"
+df = pd.read_sql(query, sql_conn)
+
+#SHOW INFORMATION ON DATAFRAME
+count_rows = df.shape[0]
+print(count_rows)
+print(df.head(10))
+
+#SAVE THE DATA PULL AS A CSV
+df.to_csv('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_40.csv')
+print('40 SUCCESSFULLY WRITTEN TO CSV')
+
+#############
+## 60 DAYS ##
+#############
+
+#PULL FROM DATABASE AND CREATE DATAFRAME
+sql_conn = pyodbc.connect('dsn=ConnectR')
+query = "select masdiv, station, tuning_evnt_start_dt, dow, moy, transactions from (SELECT mas.mas_div_desc AS MASDIV, stn.stn_nm AS STATION, tuning_evnt_start_dt, cal.day_of_week_shrt_nm AS DOW, cal.clndr_mo_nm AS MOY, COUNT(am_tuning_evnt_key) AS TRANSACTIONS FROM prd_am_bi_1.am_program_tuning_event_fact ptef INNER JOIN prd_am_bi_1.am_calendar_dim cal ON ptef.tuning_evnt_start_dt = cal.clndr_dt INNER JOIN prd_am_bi_1.am_station_dim stn ON ptef.stn_key = stn.stn_key INNER JOIN prd_am_bi_1.am_mas_division_dim mas ON (ptef.mas_div_key = mas.mas_div_key and cal.CLNDR_SKEY between mas.EFF_START_DAY_KEY and mas.EFF_END_DAY_KEY) WHERE cal.clndr_dt < current_date - 40 and cal.clndr_dt >= current_date - 60 and mas.mas_div_desc in ('Charter') GROUP BY tuning_evnt_start_dt, DOW, MOY, STATION, MASDIV) x;"
+df = pd.read_sql(query, sql_conn)
+
+#SHOW INFORMATION ON DATAFRAME
+count_rows = df.shape[0]
+print(count_rows)
+print(df.head(10))
+
+#SAVE THE DATA PULL AS A CSV
+df.to_csv('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_60.csv')
+print('60 SUCCESSFULLY WRITTEN TO CSV')
+
+#############
+## 80 DAYS ##
+#############
+
+#PULL FROM DATABASE AND CREATE DATAFRAME
+sql_conn = pyodbc.connect('dsn=ConnectR')
+query = "select masdiv, station, tuning_evnt_start_dt, dow, moy, transactions from (SELECT mas.mas_div_desc AS MASDIV, stn.stn_nm AS STATION, tuning_evnt_start_dt, cal.day_of_week_shrt_nm AS DOW, cal.clndr_mo_nm AS MOY, COUNT(am_tuning_evnt_key) AS TRANSACTIONS FROM prd_am_bi_1.am_program_tuning_event_fact ptef INNER JOIN prd_am_bi_1.am_calendar_dim cal ON ptef.tuning_evnt_start_dt = cal.clndr_dt INNER JOIN prd_am_bi_1.am_station_dim stn ON ptef.stn_key = stn.stn_key INNER JOIN prd_am_bi_1.am_mas_division_dim mas ON (ptef.mas_div_key = mas.mas_div_key and cal.CLNDR_SKEY between mas.EFF_START_DAY_KEY and mas.EFF_END_DAY_KEY) WHERE cal.clndr_dt < current_date - 60 and cal.clndr_dt >= current_date - 80 and mas.mas_div_desc in ('Charter') GROUP BY tuning_evnt_start_dt, DOW, MOY, STATION, MASDIV) x;"
+df = pd.read_sql(query, sql_conn)
+
+#SHOW INFORMATION ON DATAFRAME
+count_rows = df.shape[0]
+print(count_rows)
+print(df.head(10))
+
+#SAVE THE DATA PULL AS A CSV
+df.to_csv('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_80.csv')
+print('80 SUCCESSFULLY WRITTEN TO CSV')
+
+#############
+## 90 DAYS ##
+#############
+
+#PULL FROM DATABASE AND CREATE DATAFRAME
+sql_conn = pyodbc.connect('dsn=ConnectR')
+query = "select masdiv, station, tuning_evnt_start_dt, dow, moy, transactions from (SELECT mas.mas_div_desc AS MASDIV, stn.stn_nm AS STATION, tuning_evnt_start_dt, cal.day_of_week_shrt_nm AS DOW, cal.clndr_mo_nm AS MOY, COUNT(am_tuning_evnt_key) AS TRANSACTIONS FROM prd_am_bi_1.am_program_tuning_event_fact ptef INNER JOIN prd_am_bi_1.am_calendar_dim cal ON ptef.tuning_evnt_start_dt = cal.clndr_dt INNER JOIN prd_am_bi_1.am_station_dim stn ON ptef.stn_key = stn.stn_key INNER JOIN prd_am_bi_1.am_mas_division_dim mas ON (ptef.mas_div_key = mas.mas_div_key and cal.CLNDR_SKEY between mas.EFF_START_DAY_KEY and mas.EFF_END_DAY_KEY) WHERE cal.clndr_dt < current_date - 80 and cal.clndr_dt >= current_date - 90 and mas.mas_div_desc in ('Charter') GROUP BY tuning_evnt_start_dt, DOW, MOY, STATION, MASDIV) x;"
+df = pd.read_sql(query, sql_conn)
+
+#SHOW INFORMATION ON DATAFRAME
+count_rows = df.shape[0]
+print(count_rows)
+print(df.head(10))
+
+#SAVE THE DATA PULL AS A CSV
+df.to_csv('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_90.csv')
+print('90 SUCCESSFULLY WRITTEN TO CSV')
+
+#COMBINE THE DATA PULLS TO CREATE 90 DAY DATASET AND SAVE AS CSV
+df1 = pd.read_csv('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_20.csv')
+print('1')
+df2 = pd.read_csv('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_40.csv')
+print('2')
+df3 = pd.read_csv('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_60.csv')
+print('3')
+df4 = pd.read_csv('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_80.csv')
+print('4')
+df5 = pd.read_csv('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_90.csv')
+print('5')
+frames = [df1,df2,df3,df4,df5]
+super = pd.concat(frames)
+count_rows = super.shape[0]
+print(count_rows)
+super.to_csv('C:\\Users\\p2813634\\Development\\python\\teradata\\chtr_super.csv')
+print('SUPERSET SUCCESSFULLY WRITTEN TO CSV')
---
+++ nonchtr_DataInsert.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import teradata
+import datetime
+from datetime import date, timedelta
+
+currDate = date.today()
+yyyymmdd = currDate.strftime('%Y%m%d')
+
+
+#df = pd.read_csv('/home/ec2-user/fish/git/aws/anomaly_nonchtr_20181016.csv')
+df = pd.read_csv('C:\\Users\\p2813634\\Development\\python\\data\\anomaly_nonchtr_20181017.csv')
+print('CSV LOADED')
+count_rows = df.shape[0]
+print(df.head(5))
+print(count_rows)
+
+###############################################
+## Data Type Manipulations to match Teradata ##
+###############################################
+
+#DATE ALGORITHM IS RUN AND RECORDS ARE SCORED
+#RUN_DT = datetime.date(2018, 10, 18) #year, month, today
+RUN_DT = datetime.datetime.today().strftime('%Y-%m-%d')
+print(RUN_DT)
+
+#DATE RECORD ADDED TO TERADATA TABLE
+#CREATED_DT = datetime.date(2018, 10, 18) #year, month, today
+CREATED_DT = datetime.datetime.today().strftime('%Y-%m-%d')
+
+#ADJUST DATE FORMAT ON TUNING_EVNT_START_DT
+df['TUNING_EVNT_START_DT'] = pd.to_datetime(df.TUNING_EVNT_START_DT)
+df['TUNING_EVNT_START_DT'] = df['TUNING_EVNT_START_DT'].dt.strftime('%Y-%m-%d')
+
+#SET TRANSACTION TYPE
+#TRXTYPE = 'chtr'
+TRXTYPE = 'nonchtr'
+#print(TRXTYPE)
+
+#CHANGE SCORES TO FLOAT
+df['scores'] = df['scores'].astype(float)
+
+#CHANGE ANOMALY COLUMNS TO INTO
+df['anomaly'] = df['anomaly'].astype(int)
+df['t_anomaly'] = df['t_anomaly'].astype(int)
+
+df['TRXTYPE'] = 'nonchtr'
+df['CREATED_DT'] = CREATED_DT
+df['RUN_DT'] = RUN_DT
+df['ANOMALY_FLAG'] = 1
+
+df = df.drop(['DOW_INT','MOY_INT','DT_NBR'], axis=1)
+df.columns = df.columns.str.upper()
+
+df = df[['TRXTYPE','CREATED_DT','RUN_DT','MASDIV','STATION','TUNING_EVNT_START_DT','DOW','MOY','TRANSACTIONS',
+         'SCORES','ANOMALY','T_ANOMALY','ANOMALY_FLAG']]
+
+#######################
+## SQLALCHEMY INSERT ##
+#######################
+
+from sqlalchemy import create_engine
+user = 'E813634'
+pwd = 'DQX1duds!'
+host = '142.136.184.160'
+
+td_engine = create_engine('teradata://'+ user +':' + pwd + '@'+ host + '/' + '?authentication=LDAP?database=DLABBUAnalytics_Lab')
+
+print(df.head())
+print(df.info())
+
+df.to_sql(con=td_engine, name='Anomaly_Detection_SuperSet', if_exists='append', index=False, schema='DLABBUAnalytics_Lab')
+
+df2 = pd.read_sql("select * from DLABBUAnalytics_Lab.Anomaly_Detection_SuperSet", td_engine)
+
+print(df2)
